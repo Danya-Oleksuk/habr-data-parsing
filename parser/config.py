e5d13fb94ca9be8dc0d0d4cb8c13772de10c7a9b
Danya Oleksuk <sinrandomg@gmail.com>
2025-09-01 16:56:53 +0300

    Apply black formatting

--- parser/config.py
+++ parser/config.py
@@ -1,6 +1,6 @@
 BASE_URL = "https://habr.com/ru/hubs/python/articles/page{}/"
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
 }
 MAX_PAGES = 50
 EXCEL_FILE = "parsed_data.xlsx"
